7d9bde2262ce05017c5d4926a3a4db1e76287260
anna-masl <anna.maslinnikova@gmail.com>
2024-10-01 12:37:28 +0300

    fix condition

--- snc/app/MainHomeWork05.java
+++ snc/app/MainHomeWork05.java
@@ -13,7 +13,7 @@ public class MainHomeWork05 {
         if (income > 0 && income <= 10000) {
             tax = income * 0.025;
             System.out.println("Your tax will be: " + tax);
-        } else if (income > 10000 && income <= 250000){
+        } else if (income > 10000 && income <= 25000){
             tax = income * 0.045;
             System.out.println("Your tax will be: " + tax);
         } else if (income > 25000){
